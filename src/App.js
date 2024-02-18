546b7e36820c9502a2376ebaad5e581c976e7191
Dawan Sawyer <dawansawyer@gmail.com>
2024-02-18 13:18:43 -0500

    feat: Import HomePage and ProviderDetails components Imported and set up routes for the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './components/HomePage';
+import ProviderDetails from './components/ProviderDetails';
+
 function App() {
   return (
-    <div className="App">
-        <h1>Hello world</h1>
-    </div>
+    <Router>
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/provider/:id" element={<ProviderDetails />}/>
+
+            </Routes>
+        </div>
+    </Router>
   );
 }
 
