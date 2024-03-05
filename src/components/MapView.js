b460e6bdbfcae8e6de2382a62e980c02c3127d92
Dawan Sawyer <dawansawyer@gmail.com>
2024-03-05 16:09:44 -0500

    Enhance MapView with dynamic marker updates and auto-zoom Implement state hooks  and  in MapView for map instance and markers array to manage and retain their state across renders. Add useEffect to initialize map upon component mount and clean up by removing map and markers on component unmount to prevent memory leaks. Introduce useEffect to dynamically create markers on the map whenever the providers prop changes, which represents a new search submission. Adjust map view bounds to include all new markers, providing an auto-zoom feature that ensures all search results are visible within the map view. Accept , , and  as props to the MapView to enable conditional rendering and updating of markers based on user searches.
    This update enriches the interactivity of the map by dynamically responding to user searches and providing a clear visual representation of the search results.

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,32 +1,72 @@
+import "../styles/styles.css";
 import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { fetchProviders } from "../services/providerService";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-const MapView = ({ providers }) => {
+const MapView = ({ providers, searchAttribute, searchTerm, isSearchSubmitted, setIsSearchSubmitted  }) => {
   const mapContainerRef = useRef(null);
+  const mapRef = useRef(null);
+  const markersRef = useRef([]); 
 
   useEffect(() => {
-    const map = new mapboxgl.Map({
+    mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [-74.006, 40.7128],
       zoom: 12,
     });
 
+    return () => {
+      markersRef.current.forEach((marker) => marker.remove()); 
+      mapRef.current?.remove(); 
+    };
+  }, []);
+
+
+  useEffect(() => {
+    if (providers && isSearchSubmitted && searchAttribute && searchTerm) {
+      fetchProviders(searchAttribute, searchTerm).then(newProviders => {
+        providers(newProviders); 
+        setIsSearchSubmitted(false); 
+      }).catch(error => {
+        console.error('Error fetching providers:', error);
+      });
+    }
+  }, [providers, searchAttribute, searchTerm, isSearchSubmitted, setIsSearchSubmitted ]);
+
+  useEffect(() => {
+    if (!mapRef.current || providers.length === 0) return;
+
+    markersRef.current.forEach((marker) => marker.remove());
+    markersRef.current = [];
+
+    const bounds = new mapboxgl.LngLatBounds();
+
     providers.forEach((provider) => {
-      new mapboxgl.Marker()
-        .setLngLat([provider.longitude, provider.latitude])
-        .setPopup(
-          new mapboxgl.Popup({ offset: 30 }).setHTML(
-            `<h3>${provider.practice_name}</h3><p>${provider.practice_mailing_address}</p>`
+      if (provider.longitude && provider.latitude) {
+        bounds.extend([provider.longitude, provider.latitude]);
+        const el = document.createElement("div");
+        el.className = "marker";
+
+        const marker = new mapboxgl.Marker(el, { anchor: "bottom" })
+          .setLngLat([provider.longitude, provider.latitude])
+          .setPopup(
+            new mapboxgl.Popup({ offset: 30 }).setHTML(
+              `<h3>${provider.practice_name}</h3><p>${provider.practice_mailing_address}</p>`
+            )
           )
-        )
-        .addTo(map);
+          .addTo(mapRef.current);
+
+        markersRef.current.push(marker);
+      }
     });
 
-    return () => map.remove();
+    if (bounds.isEmpty() === false) {
+      mapRef.current.fitBounds(bounds, { padding: 50 });
+    }
   }, [providers]);
 
   return (
