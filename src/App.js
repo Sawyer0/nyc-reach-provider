c28c5de2c864a802af1a7b57d4df30255db91fcc
Dawan Sawyer <dawansawyer@gmail.com>
2024-03-10 17:55:05 -0400

    Streamline routing by removing ProviderDetails route Remove the import and route for  from  component. Simplify the React Router setup by only including the  component route. This change reflects a decision to focus the application's initial load on the homepage, possibly for a clearer user navigation or redesign of the app routing structure.
    These modifications streamline the initial routing setup, making  the sole accessible component through the main route.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from './components/HomePage';
-import ProviderDetails from './components/ProviderDetails';
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <Router>
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<HomePage />}/>
-                <Route path="/provider/:id" element={<ProviderDetails />}/>
-
-            </Routes>
-        </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
