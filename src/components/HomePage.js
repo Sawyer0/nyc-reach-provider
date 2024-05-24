77f96f3fa9791ce02cb5ac6841e32edb3033c77e
Dawan Sawyer <dawansawyer@gmail.com>
2024-05-24 11:23:21 -0400

    Refactor: Move provider list rendering logic from HomePage to ProviderDetails Extracted provider list rendering logic from HomePage component. Created a new ProviderDetails component to handle the display of provider information. Updated HomePage to use the new ProviderDetails component, passing provider data as props. This refactor improves code organization by separating concerns between components.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./Header";
 import SearchForm from "./SearchForm";
 import MapView from "./MapView";
+import ProviderDetails from "./ProviderDetails";
 
 const HomePage = (searchAttribute, searchTerm) => {
   const [providers, setProviders] = useState([]);
@@ -14,25 +15,7 @@ const HomePage = (searchAttribute, searchTerm) => {
         setProviders={setProviders}
         setIsSearchSubmitted={setIsSearchSubmitted}
       />
-      <div>
-        {providers.map((provider) => (
-          <div
-            key={`${provider.provider_first_name}-${provider.provider_last_name}-${provider.primary_specialty}-${provider.practice_borough}`}
-          >
-            <ul>
-              <h3>
-                {provider.provider_first_name} {provider.provider_last_name}
-              </h3>
-              <li>{provider.site_name}</li>
-              <li>{provider.organization_type}</li>
-              <li>{provider.primary_speciality}</li>
-              <li>{provider.practice_name}</li>
-              <li>{provider.practice_mailing_address}</li>
-              {provider.practice_zip_code} {provider.practice_borough}
-            </ul>
-          </div>
-        ))}
-      </div>
+      <ProviderDetails providers={providers} />
       <MapView
         searchAttribute={searchAttribute}
         searchTerm={searchTerm}
--- src/components/ProviderDetails.js
+++ src/components/ProviderDetails.js
@@ -1,11 +1,25 @@
-import React from 'react'
-
-const ProviderDetails = () => {
+const ProviderDetails = ({ providers }) => {
   return (
     <div>
-      <h1>Provider Details</h1>
+      {providers.map((provider) => (
+        <div
+          key={`${provider.provider_first_name}-${provider.provider_last_name}-${provider.primary_specialty}-${provider.practice_borough}`}
+        >
+          <ul>
+            <h3>
+              {provider.provider_first_name} {provider.provider_last_name}
+            </h3>
+            <li>{provider.site_name}</li>
+            <li>{provider.organization_type}</li>
+            <li>{provider.primary_speciality}</li>
+            <li>{provider.practice_name}</li>
+            <li>{provider.practice_mailing_address}</li>
+            {provider.practice_zip_code} {provider.practice_borough}
+          </ul>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProviderDetails
+export default ProviderDetails;
